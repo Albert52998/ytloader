2bb4d49d6984dcc4bcbb16a942722663df3287a5
Albert52998 <albert.harutyunyan52998@mail.ru>
2020-09-28 02:25:17 +0400

    All it works!!!

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import index, video_loader
 
 urlpatterns = [
-	path('video/<int:video_id>/', video_loader, name='video_loader'),
+	path('video/download/', video_loader, name='video_loader'),
+	# path('video/', v_download, name='v_download'),
 	path('', index, name='home'),
 ]
